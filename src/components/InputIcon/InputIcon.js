7b1c7e2bc6cc1b94cf5772121a02a21955ad8786
Wilmar Valencia <wilmar2545@gmail.com>
2024-12-05 11:05:36 -0500

    Remove unused imports

--- src/components/InputIcon/InputIcon.js
+++ src/components/InputIcon/InputIcon.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { PaperAirplaneIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
 const InputIcon = ({ className, children, placeholder, inputStyle }) => {
